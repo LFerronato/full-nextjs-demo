57548c60bd2973321d7b6aae7bb488e5d8dc17e0
Lucas Ferronato <lucas.fe.pelle@gmail.com>
2023-04-09 00:26:19 -0300

    chore(404-page): add layout and some adjusts

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,12 @@
+import _Layout from "@/domains/layout";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function NotFound() {
+const NotFound: NextPageWithLayout = () => {
   return (
     <div
       className="container"
-      style={{ textAlign: 'center', minHeight: '100vh', paddingTop: '1rem', }}
+      style={{ textAlign: 'center', paddingTop: '1rem', }}
     >
       <h1>Oops! 404</h1>
       <p>Oops! The page you requested could not be found.</p>
@@ -16,6 +18,12 @@ export default function NotFound() {
           style={{ borderRadius: '1rem' }}
         />
       </div>
+
+      <Link href="/" style={{ display: 'block', margin: '1rem 0' }}>Voltar para página inicial</Link>
     </div>
   )
 }
+
+NotFound.layout = _Layout
+
+export default NotFound
