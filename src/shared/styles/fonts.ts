d228c3253da96ad2984c5aec2239828e2b8c5669
Lucas Ferronato <lucas.fe.pelle@gmail.com>
2023-04-11 22:56:02 -0300

    fix: custom fonts cant be in public folder

--- src/shared/styles/fonts.ts
+++ src/shared/styles/fonts.ts
@@ -2,4 +2,4 @@ import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 
 export const interFont = Inter({ subsets: ['latin'] })
-export const cubanoFont = localFont({ src: '../../../public/fonts/Cubano-Regular.otf' })
+export const cubanoFont = localFont({ src: './fonts/Cubano-Regular.otf' })
