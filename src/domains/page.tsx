aa7651d200bf870688e24597be9e8c0369de07c0
Lucas Ferronato <lucas.fe.pelle@gmail.com>
2023-04-11 23:15:32 -0300

    chore(home): import explanation of SSG

--- src/domains/page.tsx
+++ src/domains/page.tsx
@@ -5,7 +5,24 @@ const HomePage = () => {
     <div style={{ textAlign: 'center' }}>
       <h1 className={cubanoFont.className} style={{ margin: '4rem 0', fontSize: '3rem' }}>Hey dev! 🚀</h1>
 
-      <p>Default strategy of rendering is <b>SSG</b> (Static Site Generation) - at build-time</p>
+      <p>Estratégia de renderização padrão: <b>SSG</b> (Static Site Generation) - at build-time</p>
+
+      <pre style={{ textAlign: 'left' }}>
+        {`
+          $ npm run build
+
+          Route (pages)                                     Size
+          ┌ ○ /                                             1.17 kB
+          ├ ○ /404                                          1.25 kB
+          ├ λ /api/hello                                    0 B
+          ├ ○ /samples/next-image                           2.88 kB
+          └ λ /samples/strategy-ssr/[name]                  1.8 kB
+          + First Load JS shared by all                     85.3 kB
+
+          λ  (Server)  server-side renders at runtime (uses getInitialProps or getServerSideProps)
+          ○  (Static)  automatically rendered as static HTML (uses no initial props)
+        `}
+      </pre>
     </div>
   )
 }
